42cb7be33ac02e0361c4d9dbc9b49de0e4c9f63f
hassan ali <hassanali0111272@gmail.com>
2024-12-28 16:30:27 +0500

    in progress

--- weather.js
+++ weather.js
@@ -1,15 +1,24 @@
 const form = document.querySelector(".search-section");
 let cityName = document.querySelector("#cityName");
-let body = document.querySelector("body")
+let body = document.querySelector("body");
+
+const tem = document.querySelector(".tem")
+
+const api_key = `417d1e8b3b244d6d891105640242812`;
 
 
-form.addEventListener("submit",()=>{
-  event.preventDefault()
+form.addEventListener("submit", () => {
+  event.preventDefault();
+  getWeatherData(cityName.value)
+ 
+});
 
-  body.style.backgroundColor = "lightblue"
-})
+async function getWeatherData (city_name){
+  // console.log(city_name)
+   let url = `http://api.weatherapi.com/v1/current.json?key=${api_key}&q=${city_name}&aqi=no`;
+   let response = await fetch (url)
+   let data = await response.json()
+   console.log(data)
 
-// function sub (){
-//   event.preventDefault()
-//   alert(cityName.value)
-// }
+   tem.innerHTML = data.current.temp_c;
+}
